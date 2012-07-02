50a4d6586e96ca40da32f0aa8ddf27e094bc127e
Jimexist <jimexist@gmail.com>
2012-07-02 13:27:00 +0800

    delete added to AVLTree

--- Tree/AVLTree.hs
+++ Tree/AVLTree.hs
@@ -3,8 +3,14 @@ module Tree.AVLTree
 , singleton
 , heightOf
 , insert
+, delete
+, findMax
+, findMin
 , isBalanced
+, isBalancedRecursive
     ) where
+
+import Data.Maybe (fromJust)
 
 data AVLTree k v = Empty | Branch { left :: AVLTree k v
                                   , pair :: (k, v)  
@@ -23,25 +29,49 @@ heightOf branch = h branch
 insert :: (Ord k) => (k, v) -> AVLTree k v -> AVLTree k v
 insert (key, val) Empty = singleton (key, val)
 insert (key, val) (Branch leftNode (k, v) h rightNode) = case compare key k of
-                                                    -- update
-                                                    EQ -> Branch leftNode (key, val) h rightNode
-                                                    -- insert right
-                                                    GT -> let insertResult = Branch leftNode (k, v) h (insert (key, val) rightNode) in
-                                                            if isBalanced insertResult then
-                                                                updateHeight insertResult
-                                                            else -- rotate
-                                                                case compare key (fst $ pair rightNode) of
-                                                                    GT -> updateHeight . rotateRightSingle $ insertResult
-                                                                    LT -> updateHeight . rotateRightDouble $ insertResult
-                                                    -- insert left
-                                                    LT -> let insertResult = Branch (insert (key, val) leftNode) (k, v) h rightNode in
-                                                            if isBalanced insertResult then
-                                                                updateHeight insertResult
-                                                            else -- rotate
-                                                                case compare key (fst $ pair leftNode) of
-                                                                    LT -> updateHeight . rotateLeftSingle $ insertResult
-                                                                    GT -> updateHeight . rotateLeftDouble $ insertResult
+    -- update
+    EQ -> Branch leftNode (key, val) h rightNode
+    -- insert right
+    GT -> let insertResult = Branch leftNode (k, v) h (insert (key, val) rightNode) in
+            if isBalanced insertResult then
+                updateHeight insertResult
+            else -- rotate
+                case compare key (fst $ pair rightNode) of
+                    GT -> updateHeight . rotateRightSingle $ insertResult
+                    LT -> updateHeight . rotateRightDouble $ insertResult
+    -- insert left
+    LT -> let insertResult = Branch (insert (key, val) leftNode) (k, v) h rightNode in
+            if isBalanced insertResult then
+                updateHeight insertResult
+            else -- rotate
+                case compare key (fst $ pair leftNode) of
+                    LT -> updateHeight . rotateLeftSingle $ insertResult
+                    GT -> updateHeight . rotateLeftDouble $ insertResult
 
+delete :: (Ord k, Eq v) => k -> AVLTree k v -> AVLTree k v
+delete key Empty = Empty
+delete key (Branch leftNode (k, v) h rightNode) = case compare key k of
+    GT -> updateHeight $ Branch leftNode (k, v) h (delete key rightNode)
+    LT -> updateHeight $ Branch (delete key leftNode) (k, v) h rightNode
+    EQ -> if leftNode == Empty then rightNode -- both Empty then rightNode yields Empty
+          else if rightNode == Empty then leftNode
+          else let deleteResult = updateHeight $ Branch (delete (fst prev) leftNode) prev h rightNode where prev=fromJust.findMax$leftNode in -- find the previous one
+            if isBalanced deleteResult then
+                deleteResult
+            else case compare (heightOf.left.right$deleteResult) (heightOf.right.right$deleteResult) of
+                GT -> rotateRightDouble deleteResult
+                LT -> rotateRightSingle deleteResult
+            
+
+findMax :: (Ord k, Eq v) => AVLTree k v -> Maybe (k, v)
+findMax Empty = Nothing
+findMax (Branch _ (k, v) _ rightNode) | rightNode == Empty = Just (k, v)
+                                      | otherwise = findMax rightNode
+
+findMin :: (Ord k, Eq v) => AVLTree k v -> Maybe (k, v)
+findMin Empty = Nothing
+findMin (Branch leftNode (k, v) _ _) | leftNode == Empty = Just (k, v)
+                                     | otherwise = findMin leftNode
 
 rotateLeftSingle :: AVLTree k v -> AVLTree k v
 rotateLeftSingle (Branch leftNode (k, v) h rightNode) = updateHeight $ Branch (left leftNode) (pair leftNode) h (updateHeight $ Branch Empty (k, v) (h-1) rightNode)
@@ -61,4 +91,8 @@ updateHeight (Branch leftNode (k, v) _ rightNode) = Branch leftNode (k, v) h rig
 
 isBalanced :: AVLTree k v -> Bool
 isBalanced Empty = True
-isBalanced (Branch leftNode _ _ rightNode) = abs (heightOf leftNode - heightOf rightNode) <= 1 && isBalanced leftNode && isBalanced rightNode
+isBalanced (Branch leftNode _ _ rightNode) = abs (heightOf leftNode - heightOf rightNode) <= 1 -- non-recursive
+
+isBalancedRecursive :: AVLTree k v -> Bool
+isBalancedRecursive Empty = True
+isBalancedRecursive (Branch leftNode _ _ rightNode) = abs (heightOf leftNode - heightOf rightNode) <= 1 && isBalancedRecursive leftNode && isBalancedRecursive rightNode
